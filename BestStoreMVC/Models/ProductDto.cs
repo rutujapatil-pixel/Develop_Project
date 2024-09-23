e8933d0f5819457acd356e9988b2d21f35afcb8a
Rushikesh <Rushikesh@LAPTOP-HGFJ93EA>
2024-09-23 10:44:26 +0530

    Add project files.

---
+++ BestStoreMVC/Models/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BestStoreMVC.Models
+{
+    public class ProductDto
+    {
+
+        [Required, MaxLength(100)] public string Name { get; set; } = "";
+        [Required, MaxLength(100)] public string Brand { get; set; } = "";
+        [Required, MaxLength(100)] public string Category { get; set; } = "";
+        public decimal Price { get; set; }
+        public string Description { get; set; } = "";
+        public IFormFile? ImageFile { get; set; }
+    }
+}
---
+++ BestStoreMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BestStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BestStoreMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
